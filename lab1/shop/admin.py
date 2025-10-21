ae0fc3e8f502f993aaa979545a8c19b4624d06a6
0Orchlon <sw22d045@mandakh.edu.mn>
2025-10-21 17:11:18 +0800

    test ignore

--- lab1/shop/admin.py
+++ lab1/shop/admin.py
@@ -7,3 +7,5 @@ from django.contrib import admin
 # class ProductAdmin(admin.ModelAdmin):
 #     prepopulated_fields = {'slug':('product_name',)}
 # admin.site.register(Product, ProductAdmin)
+
+# fefd
